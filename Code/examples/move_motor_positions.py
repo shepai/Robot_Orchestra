94c258ee7035e64aae8ab0c559761e565ab1319b
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-02-28 14:36:55 +0000

    Update move_motor_positions.py

--- Code/examples/move_motor_positions.py
+++ Code/examples/move_motor_positions.py
@@ -2,12 +2,11 @@ import time
 from droid_class import Droid
 
 d=Droid()
-array=[150,150,100,110,50,40,100,180,180,40,180,40,50,100]
-d.set_specific(1,100)
+array=[150,150,100,110,50,40,100,180,180,40,180,40,50,100] #set arms by side
 d.setMotors(array)
 time.sleep(1)
-d.setMotors([180 for i in range(len(array))])
-time.sleep(1)
 mov=[180 for i in range(len(array))]
-mov[7]=50
+d.setMotors(mov) #set them all to highest positions
+time.sleep(1)
+mov[7]=50 #change just one of them
 d.setMotors(mov)
