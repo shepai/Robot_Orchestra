48e1a1c640a20589165acf95c6e8369844dfdf9f
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-02-29 15:46:14 +0000

    Update wifi.py

--- Code/examples/wifi.py
+++ Code/examples/wifi.py
@@ -19,3 +19,30 @@ pool = socketpool.SocketPool(wifi.radio)
 print("My IP address is", wifi.radio.ipv4_address)
 
 
+
+# Specify the server's IP address and port
+server_ip = "192.168.1.27"  # Replace with the server's IP address
+server_port = 12345
+
+# Create a socket using the pool
+sock = pool.socket()
+addr = pool.getaddrinfo(server_ip, server_port)[0][-1]
+sock.connect(addr)
+sock.send(b"Hello, server!")
+
+while True:
+    # Send data to the server
+    data = bytearray(20)    
+    a = sock.recvfrom_into(data)
+    command=data.decode("uft-8")
+    command=command.replace(" ","")
+    if "exit" in command:
+        break
+    elif "set" in command:
+        command=command.replace("set")
+        
+    else:
+        print(command)
+    # Close the socket
+sock.close()
+
