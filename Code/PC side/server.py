5388c3e11da1eeb99a3d22b23c916aa62243bea9
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-03-01 10:40:18 +0000

    Update server.py

--- Code/PC side/server.py
+++ Code/PC side/server.py
@@ -26,7 +26,23 @@ while time.time()-t<1000:
     # Send a message to the client
     try:
         message = input("Command to robot>")
-        client_socket.send(message.encode())
+        if message=="1": #command
+            client_socket.send(("set"+str([100 for i in range(14)])).encode())
+        elif message=="2":
+            a=[]
+            for i in range(14): #allo the user to manually enter values
+                a.append(int(input("Motor "+str(i+1)+" value>")))
+            client_socket.send(("set"+str([100 for i in range(14)])).encode())
+        elif message=="3": #forard
+            client_socket.send(("forward").encode())
+        elif message=="4": #stop
+            client_socket.send(("stop").encode())
+        elif message=="5": #left
+            client_socket.send(("left").encode())
+        elif message=="6": #right
+            client_socket.send(("right").encode())
+        else:
+            client_socket.send(message.encode())
         time.sleep(1)
     except ConnectionResetError:
         client_socket, client_address = server_socket.accept()
