cc316e10cdf83a32950add308f5c505ad1d6faaa
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-03-01 10:40:11 +0000

    Update wifi.py

--- Code/examples/wifi.py
+++ Code/examples/wifi.py
@@ -6,7 +6,7 @@ import time
 from droid_class import Droid
 
 d=Droid()
-
+d.neutral()
 print("Connecting to WiFi")
 
 #  connect to your SSID
@@ -31,29 +31,39 @@ sock.connect(addr)
 sock.send(b"Hello, server!")
 
 while True:
-    # Send data to the server
-    data = bytearray(20)    
-    a = sock.recvfrom_into(data)
-    command=data.decode("uft-8")
-    command=command.replace(" ","")
-    if "exit" in command:
-        break
-    elif "set" in command:
-        command=command.replace("set","").replace("[","").replace("]","")
-        ar=command.split(",")
-        mov=[int(ar[i]) for i in range(len(ar))]
-        d.setMotors(mov)
-    elif "forward" in command:
-        d.forward(0.5)
-    elif "stop" in command:
-        d.stop()
-    elif "left" in command:
-        d.left(0.5)
-    elif "right" in command:
-        d.left(0.5)
-    else:
-        print(command)
-    # Close the socket
+    try:
+        # Send data to the server
+        data = bytearray(100)    
+        a = sock.recvfrom_into(data)
+        command=data.decode("uft-8")
+        command=command.replace(" ","").rstrip('\x00')
+        print("command:",command)
+        if "exit" in command:
+            break
+        elif "set" in command:
+            command=command.replace("set","").replace("[","").replace("]","")
+            ar=command.split(",")
+            print(ar)
+            mov=[int(ar[i]) for i in range(len(ar))]
+            print(mov)
+            d.setMotors(mov)
+        elif "forward" in command:
+            d.forward(0.5)
+        elif "stop" in command:
+            d.stop()
+        elif "left" in command:
+            d.left(0.5)
+        elif "right" in command:
+            d.right(0.5)
+        elif "backward" in command:
+            d.backward(0.5)
+        else:
+            print(command)
+    except OSError:
+        sock = pool.socket()
+        addr = pool.getaddrinfo(server_ip, server_port)[0][-1]
+        sock.connect(addr)
+# Close the socket
 d.stop()
 d.neutral()
 sock.close()
